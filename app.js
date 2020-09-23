667397dd707cbbfe9690b6004353b56ddca83210
shota <shotagishitomi@gmail.com>
2020-09-23 20:39:09 +0900

    コンポーネント学習完了
    2020/09/23

--- app.js
+++ app.js
@@ -20,3 +20,29 @@ Vue.component("page-content", {
 new Vue({
   el: "#fruits-list",
 });
+
+Vue.component("user-login", {
+  template: "#login-template",
+  data: function () {
+    return {
+      id: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginBtn: function () {
+      auth.login(this.id, this.password);
+    },
+  },
+});
+
+// ログイン試行のダミー
+var auth = {
+  login: function (id, password) {
+    window.alert("userid: " + id + "\n" + "password: " + password);
+  },
+};
+
+new Vue({
+  el: "#login-example",
+});
